93962a6a690cbeb4152086b85606978e90b8b91f
yoryi andres cortes <ghoryiandres@gmail.com>
2023-12-02 16:00:19 -0500

    segundo commit BackEnd

--- index.js
+++ index.js
@@ -1,61 +1,86 @@
-import express from "express";
+import express from "express"
 import fs from "fs"
-import bodyParser from "body-parser";
-//Intancia de Express
-const app = express();
-app.use(bodyParser.json());
-//Configurar Puerto
-const port = 3000;
-// funcion leer datos
+import bodyParser from "body-parser"
 
+//Intancia de Express
+const app = express()
+
+//Configurar Puerto
+const port = 3000
+
+//Funcion Leer Datos
 const leerDatos = () => {
   try {
-    const data = fs.readFileSync("./db.json");
-    return JSON.parse(data);
+    const data = fs.readFileSync("./db.json")
+    return JSON.parse(data)
   } catch (error) {
-    console.log(`Error al leer datos >> ${error}`);
+    console.log(`Error al leer datos >> ${error}`)
   }
 }
-// funcion Escribir datos
-const escribirDatos = () => {
-  try {
-    fs.writeFileSync("./db.json", JSON.stringify(data));
-  } catch (error) {
-    console.log(error);
-  }
 
+//Funcion Escribir Datos
+const escribirDatos = data => {
+  try {
+    fs.writeFileSync("./db.json", JSON.stringify(data))
+  } catch (error) {
+    console.log(error)
+  }
 }
-//Configurar Ruta
-app.get("/", (req, res) => {
-  res.send("Bienvenido a mi proyecto de Libros");
-});
-// GET
+
+app.use(bodyParser.json())
+
+//GET
 app.get("/libros", (req, res) => {
-  const data = leerDatos();
+  const data = leerDatos()
   res.json(data.libros)
-});
-// GET libro ID
+})
+
+//GET LIBRO ID
 app.get("/libros/:id", (req, res) => {
-  const data = leerDatos();
-  const id = parseInt(req.params.id);
-  const libro = data.libros.find((libro) => libro.id === id)
+  const data = leerDatos()
+  const id = parseInt(req.params.id)
+  const libro = data.libros.find(libro => libro.id === id)
   res.json(libro)
-});
+})
 
 //POST
 app.post("/libros", (req, res) => {
-  const data = leerDatos();
-  const body = req.body;
+  const data = leerDatos()
+  const body = req.body
   const nuevoLibro = {
     id: data.libros.length + 1,
     ...body
-  };
-  data.libros.push(nuevoLibro);
-  escribirDatos(data);
-  res.json(nuevoLibro);
-});
+  }
+  data.libros.push(nuevoLibro)
+  escribirDatos(data)
+  res.json(nuevoLibro)
+})
+
+//PUT
+app.put("/libros/:id", (req, res) => {
+  const data = leerDatos()
+  const body = req.body
+  const id = parseInt(req.params.id)
+  const indiceLibro = data.libros.findIndex(libro => libro.id === id)
+  data.libros[indiceLibro] = {
+    ...data.libros[indiceLibro],
+    ...body
+  }
+  escribirDatos(data)
+  res.json({ mensaje: "Libro Actualizado con éxito" })
+})
+
+//DELETE
+app.delete("/libros/:id", (req, res) => {
+  const data = leerDatos()
+  const id = parseInt(req.params.id)
+  const indiceLibro = data.libros.findIndex(libro => libro.id === id)
+  data.libros.splice(indiceLibro, 1)
+  escribirDatos(data)
+  res.json({ mensaje: `Registro en la posicion ${indiceLibro} Eliminado Correctamente` })
+})
 
 //Escuchar app por puerto 3000
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
